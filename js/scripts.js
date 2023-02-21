b513b36dcd26f3ab4390fc7de1e4dcd105a16659
Fangfang Chen <ffchen.ca@gmail.com>
2023-02-21 15:42:49 -0500

    Upload entire files.

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+/*!
+ * Start Bootstrap - Scrolling Nav v5.0.5 (https://startbootstrap.com/template/scrolling-nav)
+ * Copyright 2013-2022 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-scrolling-nav/blob/master/LICENSE)
+ */
+//
+// Scripts
+// 
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+//
+// ( greeting
+const hour = new Date().getHours();
+let greeting;
+
+if (hour < 17) {
+    greeting = "Good day! Welcome to me page";
+} else {
+    greeting = "Good evening! Welcome to me page";
+}
+document.getElementById("demo1").innerHTML = greeting;
+// greeting END 
+
+//send email
+function myFunction() {
+    location.href = "mailto:someone@example.com";
+}
+//END send email
